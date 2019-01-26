ad940649e46111c1d450951b40c7f34d78c438c4
Lindca <45475418+Lindca@users.noreply.github.com>
2019-01-26 11:25:19 -0800

    fix instructions

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -58,150 +58,150 @@ var unansweredQuestions = [];
 $("#Q1").on("click", function (event) {
     if (event.target.value == "Q1b") {
         correctArray.push(event.target.value);
-        $("#Q1").hide();
+        $("#Q1").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q1").hide();
+        $("#Q1").remove();
     }
 })
 $("#Q2").on("click", function (event) {
     if (event.target.value == "Q2c") {
         correctArray.push(event.target.value);
-        $("#Q2").hide();
+        $("#Q2").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q2").hide();
+        $("#Q2").remove();
     }
 })
 $("#Q3").on("click", function (event) {
     if (event.target.value == "Q3d") {
         correctArray.push(event.target.value);
-        $("#Q3").hide();
+        $("#Q3").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q3").hide();
+        $("#Q3").remove();
     }
 })
 $("#Q4").on("click", function (event) {
     if (event.target.value == "Q4c") {
         correctArray.push(event.target.value);
-        $("#Q4").hide();
+        $("#Q4").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q4").hide();
+        $("#Q4").remove();
     }
 })
 $("#Q5").on("click", function (event) {
     if (event.target.value == "Q5b") {
         correctArray.push(event.target.value);
-        $("#Q5").hide();
+        $("#Q5").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q5").hide();
+        $("#Q5").remove();
     }
 })
 $("#Q6").on("click", function (event) {
     if (event.target.value == "Q6c") {
         correctArray.push(event.target.value);
-        $("#Q6").hide();
+        $("#Q6").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q6").hide();
+        $("#Q6").remove();
     }
 })
 $("#Q7").on("click", function (event) {
     if (event.target.value == "Q7d") {
         correctArray.push(event.target.value);
-        $("#Q7").hide();
+        $("#Q7").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q7").hide();
+        $("#Q7").remove();
     }
 })
 $("#Q8").on("click", function (event) {
     if (event.target.value == "Q8b") {
         correctArray.push(event.target.value);
-        $("#Q8").hide();
+        $("#Q8").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q8").hide();
+        $("#Q8").remove();
     }
 })
 $("#Q9").on("click", function (event) {
     if (event.target.value == "Q9a") {
         correctArray.push(event.target.value);
-        $("#Q9").hide();
+        $("#Q9").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q9").hide();
+        $("#Q9").remove();
     }
 })
 $("#Q10").on("click", function (event) {
     if (event.target.value == "Q10a") {
         correctArray.push(event.target.value);
-        $("#Q10").hide();
+        $("#Q10").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q10").hide();
+        $("#Q10").remove();
     }
 })
 $("#Q11").on("click", function (event) {
     if (event.target.value == "Q11c") {
         correctArray.push(event.target.value);
-        $("#Q11").hide();
+        $("#Q11").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q11").hide();
+        $("#Q11").remove();
     }
 })
 $("#Q12").on("click", function (event) {
     if (event.target.value == "Q12c") {
         correctArray.push(event.target.value);
-        $("#Q12").hide();
+        $("#Q12").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q12").hide();
+        $("#Q12").remove();
     }
 })
 $("#Q13").on("click", function (event) {
     if (event.target.value == "Q13b") {
         correctArray.push(event.target.value);
-        $("#Q13").hide();
+        $("#Q13").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q13").hide();
+        $("#Q13").remove();
     }
 })
 $("#Q14").on("click", function (event) {
     if (event.target.value == "Q14d") {
         correctArray.push(event.target.value);
-        $("#Q14").hide();
+        $("#Q14").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q14").hide();
+        $("#Q14").remove();
     }
 })
 $("#Q15").on("click", function (event) {
     if (event.target.value == "Q15a") {
         correctArray.push(event.target.value);
-        $("#Q15").hide();
+        $("#Q15").remove();
     }
     else {
         incorrectArray.push(event.target.value);
-        $("#Q15").hide();
+        $("#Q15").remove();
     }
 })
